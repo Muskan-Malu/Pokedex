3e9541af73ef28d12718b8831ef69caed256b053
Muskan-Malu <muskanmalu2511@gmail.com>
2023-01-16 15:23:03 +0530

    First Commit

--- src/components/SurpriseMe/Battle.js
+++ src/components/SurpriseMe/Battle.js
@@ -1,36 +1,60 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import pic from '../images/Battle.png';
+import pic1 from '../images/Pokeball.png';
 import Pokeball from '../images/Pokeball.png';
 import './BattleStyles.css';
 import { Card, Button } from 'antd';
+import { ArrowLeftOutlined, ArrowRightOutlined } from '@ant-design/icons';
 import PokeballOpen from '../images/PokeballOpen.png';
 import Pokemons from '../../allPokemons.json';
 import { TypeAndWeakness, TypeSpaceContainer } from "../pokedexDetailCard/pokedexDetailsStyle.js";
 import styleType from '../../pokeTypeDesign.json';
+import { Link } from 'react-router-dom';
+import { set } from "lodash";
 
 export const Battle = () => {
     const [ abilities, setAbilities ] = useState(false);
     const [ weakness, setWeakness ] = useState(false);
     const [ click, setClick ] = useState(true);
-    const [ winner, setWinner ] = useState({})
+    const [ winner, setWinner ] = useState({});
+    const [ side, setSide ] = useState("");
+    const [ winSide, setWinSide ] = useState("");
 
     let Poke1 = Pokemons[Math.floor(Math.random() * Pokemons.length)];
     let Poke2 = Pokemons[Math.floor(Math.random() * Pokemons.length)];
+    let winPoke = Poke1.strength > Poke2.strength ? Poke1 : Poke2;
+    let sideWinPoke = winPoke === Poke1 ? "left" : "right"; 
     if(Poke1 === Poke2) {
         Poke2 = Pokemons[Math.floor(Math.random() * Pokemons.length)];
     }
     let arr = [Poke1, Poke2];
-    let i = 1;
 
     const handleOnClick = () => {
-        setWinner(Poke1);
+        setWinSide(sideWinPoke);
+        setWinner(winPoke);
         setClick(false);
     }
+
+    console.log(side, "Your Choice");
+    console.log(sideWinPoke, "Winner Side");
+
+    let text = winPoke === side ? "Yayy! You've won🎉" : "Oopsie! Let's try again";
 
     return (
         <div className="BattleContainer">
             <div className="BattleHeader" style={{ backgroundImage: `url(${pic})` }}>
-                {click && 
+                {!side && <div className="WinnerContainer">
+                    <Card className="SideCard">
+                        <div className="SideTxt">Please pick your side</div>
+                        <div style={{ width: "400px", padding: "20px", justifyContent: "space-around", alignItems: "center" , display: "flex"}}>
+                            <Button className="SideButton" onClick={() => setSide("left")}>
+                            <ArrowLeftOutlined />Left</Button>
+                            <Button className="SideButton" onClick={() => setSide("right")}>
+                            Right<ArrowRightOutlined /></Button>
+                        </div>
+                    </Card>
+                    </div>}
+                {side && click && 
                     <>
                     <div className="set">
                     <div><img src={Pokeball} ></img></div>
@@ -91,14 +115,20 @@ export const Battle = () => {
                     })}
                 </div>
                 <div>
-                    {i === 1 && <Button className="BattleButton" onClick={handleOnClick}>Battle</Button>}
+                    <Button className="BattleButton" onClick={handleOnClick}>Battle</Button>
                 </div>
                     </>
                 }
                 {!click && winner &&
                     <>
+                    <div style={{ "padding-top": "10px", position: "relative", left: "46%" }}>
+                        <Link to={"/surpriseMe"} style={{ textDecoration: "none", color: "#edf0f5" }}>
+                        <img src={pic1} />
+                        <div style={{ fontSize: "20px" }}>Surprise Me!!</div>
+                        </Link>
+                    </div>
                         <div className="WinnerContainer">Winner!!</div>
-                        <div className="CardContainer">
+                        <div className="CardContainer1">
                         <Card className="WinnerCards" cover={<img src={winner.ThumbnailImage} style={{ width: "200px", height: "200px", position: "relative", top: "40px "}}></img>}>
                                 <div className="PokeDetails">
                                     <div style={{ fontSize: "28px", fontWeight: "800" }}>{winner.name}</div>
@@ -145,6 +175,7 @@ export const Battle = () => {
                                     </div>
                                 </div>
                             </Card>
+                            <div className="WinnerContainer1">{text}</div>
                         </div>
                     </>
                 }
